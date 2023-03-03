b2f1ff6473e547ca600936c2786d2e199230fb11
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-03 14:05:31 -0300

    Add project files.

---
+++ fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double prox = 0, numeroAnterior = 1, numeroAtual;
+        int posicao;
+
+        Console.Write("Infome um numero para a sequencia de fibonacci: ");
+        posicao = int.Parse(Console.ReadLine());
+
+
+        for(int i = 0; i < posicao; i++)
+        {
+            Console.WriteLine(numeroAnterior + prox);
+            prox = numeroAnterior + prox;
+            numeroAnterior = prox - numeroAnterior;
+        }      
+    }
+}
